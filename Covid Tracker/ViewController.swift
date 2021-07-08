109b01bd64df534c1b7d10c2eb1f7aa8fb02d60d
Ruan van der Westhuizen <rvanderwesthuizen2@jhb.dvt.co.za>
2021-07-08 16:51:52 +0200

    Initial Commit

---
+++ Covid Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid Tracker
+//
+//  Created by Ruan van der Westhuizen on 2021/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
